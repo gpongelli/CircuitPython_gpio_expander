2126f45dabc8d27303538e19f446b01189384145
gpongelli <842026+gpongelli@users.noreply.github.com>
2022-10-26 17:54:21 +0200

    comments removed

--- tests/test_pca9555.py
+++ tests/test_pca9555.py
@@ -10,14 +10,6 @@ def test_pca9555_object(mock_i2c, registry_list_16_gpio):
         assert _r in _dev_attribs
 
 
-# def test_pca9555_gpio_value(mocker, mock_i2c, registry_list_16_gpio):
-#     mocker.patch("gpio_expander.PCA9555.GPIO0.value", return_value=True)
-#     _dev = gpio_expander.PCA9555(I2C(2, 3), 4)  # fake addresses
-#
-#     #assert _dev.O0_0 is True
-#     assert _dev.GPIO0.value is True
-
-
 def test_pca9555_num_gpios(mock_i2c):
     _dev = gpio_expander.PCA9555(I2C(2, 3), 4)  # fake addresses
     assert 16 == _dev.max_gpios()
