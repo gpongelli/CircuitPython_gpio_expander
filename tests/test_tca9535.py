c0137e284db6c63ce1d09f8b0ac496951519af94
gpongelli <842026+gpongelli@users.noreply.github.com>
2022-10-26 18:33:15 +0200

    fix: missing licenses

--- tests/test_tca9535.py
+++ tests/test_tca9535.py
@@ -1,3 +1,8 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2022 Gabriele Pongelli
+#
+# SPDX-License-Identifier: MIT
+
 from busio import I2C
 
 import gpio_expander
