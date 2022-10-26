2997772eca0d11bef50da3d79397c373d6d4a53d
gpongelli <842026+gpongelli@users.noreply.github.com>
2022-10-26 18:21:21 +0200

    fix: disable warning on pytest fixture

--- tests/test_pca9534.py
+++ tests/test_pca9534.py
@@ -4,13 +4,13 @@ from busio import I2C
 import gpio_expander
 
 
-def test_pca9534_object(mock_i2c, registry_list_8_gpio):
+def test_pca9534_object(mock_i2c, registry_list_8_gpio):  # pylint: disable=unused-argument
     _dev = gpio_expander.PCA9534(I2C(2, 3), 4)  # fake addresses
     _dev_attribs = dir(_dev)
     for _r in registry_list_8_gpio:
         assert _r in _dev_attribs
 
 
-def test_pca9534_num_gpios(mock_i2c):
+def test_pca9534_num_gpios(mock_i2c):  # pylint: disable=unused-argument
     _dev = gpio_expander.PCA9534(I2C(2, 3), 4)  # fake addresses
     assert _dev.max_gpios() == 8
--- tests/test_pca9555.py
+++ tests/test_pca9555.py
@@ -4,13 +4,13 @@ from busio import I2C
 import gpio_expander
 
 
-def test_pca9555_object(mock_i2c, registry_list_16_gpio):
+def test_pca9555_object(mock_i2c, registry_list_16_gpio):  # pylint: disable=unused-argument
     _dev = gpio_expander.PCA9555(I2C(2, 3), 4)  # fake addresses
     _dev_attribs = dir(_dev)
     for _r in registry_list_16_gpio:
         assert _r in _dev_attribs
 
 
-def test_pca9555_num_gpios(mock_i2c):
+def test_pca9555_num_gpios(mock_i2c):  # pylint: disable=unused-argument
     _dev = gpio_expander.PCA9555(I2C(2, 3), 4)  # fake addresses
     assert _dev.max_gpios() == 16
