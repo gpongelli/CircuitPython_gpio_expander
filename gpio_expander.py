b763d858ad2fe2e62f3ac5348ef362436edbc88b
gpongelli <842026+gpongelli@users.noreply.github.com>
2022-10-27 12:37:04 +0200

    Bump version: 0.2.0 -> 0.2.1

--- gpio_expander.py
+++ gpio_expander.py
@@ -40,7 +40,7 @@ try:
 except ImportError:
     pass
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __repo__ = "https://github.com/gpongelli/CircuitPython_gpio_expander.git"
 
 import adafruit_bus_device.i2c_device as i2cdevice
